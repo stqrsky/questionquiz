e5fa136f7f5a61c8e597d9691ef470c7c3b42221
star <star@starbook.fritz.box>
2020-07-30 17:45:44 +0200

    mode="out-in" -> first comp removed before news in

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
+                <transition name="flip" mode="out-in">      
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-        
+                </transition>
             </div>
         </div>
     </div>
@@ -42,4 +42,8 @@
         }
     }
 </script>
+
+<style >
+  
+</style>
 
