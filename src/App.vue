7a2124b6a94152b9d4dfdd420e51e67686477799
star <star@starbook.fritz.box>
2020-07-30 17:52:20 +0200

    style

--- src/App.vue
+++ src/App.vue
@@ -45,36 +45,36 @@
 
 <style >
     .flip-enter {
-        
+        /* transform: rotateY(0deg);   */       /*starting state */
     }
 
     .flip-enter-active {
-       
+        animation: flip-in  0.5s ease-out forwards;
     }
 
     .flip-leave {
-        
+        /*transform: rotateY(0deg);*/
     }
 
     .flip-leave-active {
-        
+        animation: flip-out 0.5s ease-out forwards;
     }
 
     @keyframes flip-out {
         from {
-            
+            transform: rotateY(0deg);
         }
         to {
-            
+            transform: rotateY(90deg);
         }
     }
 
     @keyframes flip-in {
         from {
-            
+            transform: rotateY(90deg);
         }
         to {
-            
+            transform: rotateY(0deg);
         }
     }
 </style>
